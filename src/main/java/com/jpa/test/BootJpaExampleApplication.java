2dc12d7865aa65710d94ae1c40bd4f888a630ee3
Ramshek <orientbits@gmial.com>
2022-09-28 08:13:00 +0530

    CRUD Operation using JPA and Spring Boot | CREATE | READ | UPDATE | DELETE

--- src/main/java/com/jpa/test/BootJpaExampleApplication.java
+++ src/main/java/com/jpa/test/BootJpaExampleApplication.java
@@ -11,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class BootJpaExampleApplication {
@@ -21,15 +24,83 @@ public class BootJpaExampleApplication {
 
         UserRepository userRepository = context.getBean(UserRepository.class);
 
-        User user = new User();
-        user.setName("Ramshek Rana");
-        user.setCity("Hazaribagh");
-        user.setStatus("I am java programmer");
+//        User user = new User();
+//        user.setName("Ramshek Rana");
+//        user.setCity("Hazaribagh");
+//        user.setStatus("I am java programmer");
+//
+//        User user1 = userRepository.save(user);
+//
+//        System.out.println(user1);
 
-        User user1 = userRepository.save(user);
+        User user2 = new User();
+        user2.setName("Rohit2");
+        user2.setCity("Delhi2");
+        user2.setStatus("Learning Front end2");
 
-        System.out.println(userRepository);
-        System.out.println(user1);
+        User user3 = new User();
+        user3.setName("Mangal");
+        user3.setCity("Banha");
+        user3.setStatus("Learning Agriculture");
+/*
+        // single entity to save
+        User resultUser = userRepository.save(user2);
+        System.out.println("Saved User "+resultUser);
+
+        // for multiple user
+        List<User> user21 = List.of(user2, user3);
+        Iterable<User> resultIterable = userRepository.saveAll(user21);
+        resultIterable.forEach(System.out::println);
+
+        */
+
+
+/*        // update the user of id 11
+        Optional<User> optional = userRepository.findById(22);
+        if (optional.isPresent()) {
+            User user = optional.get();
+            user.setName("Manoj Rana");
+            userRepository.save(user);
+            System.out.println(user);
+
+            System.out.println("Done!!");
+        }else {
+            System.out.println("User not available");
+        }
+
+        */
+
+
+ /*
+        //get all user
+        Iterable<User> all = userRepository.findAll();
+        all.forEach(System.out::println);
+
+        System.out.println("----------------second way-------------------");
+        for (User user : all) {
+            System.out.println(user);
+        }
+*/
+
+
+        // deleting the user element
+//        userRepository.deleteById(3);
+//        System.out.println("Deleted!");
+
+        Iterable<User> all = userRepository.findAll();
+        all.forEach(System.out::println);
+        // to delete all user from database
+        // userRepository.deleteAll(all);
+
+        // delete user who are not from Delhi city
+        all.forEach(user -> {
+            if (!user.getCity().equals("Delhi")){
+                userRepository.deleteById(user.getId());
+            }
+        });
+
+
+
 
     }
 
